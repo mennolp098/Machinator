86182e5bc86d4e9da56f475eeda7d2a32875c0a4
Beau Arthurs <BeauArthurs@gmail.com>
2014-11-19 22:13:52 +0100

    Beau007

--- Assets/Scripts/Controllers/EnemySpawner.cs
+++ Assets/Scripts/Controllers/EnemySpawner.cs
@@ -2,18 +2,48 @@
 using System.Collections;
 
 public class EnemySpawner : MonoBehaviour {
-	public GameObject[] Enemys;
-	public float coolDown;
+    [SerializeField]
+    private GameObject[] _enemys;
+    [SerializeField]
+    private float _enemyCoolDown;
+    [SerializeField]
+    private float _waveCoolDown;
+    [SerializeField]
+    private int _startingEnemys;
+    [SerializeField]
+    private int _enemyMultiplayer;
+    private int _maxEnemys;
+    private int _currentEnemys;
+    private int _wave;
 	// Use this for initialization
-	void Start () {
-		Invoke ("spawnEnemy", coolDown);
+	void Start () 
+    {
+        Invoke("startWave", _waveCoolDown);
+        _maxEnemys = _startingEnemys;
 	}
 	
-	// Update is called once per frame
-	void spawnEnemy () {
-		int random = Random.Range (0, Enemys.Length);
-		GameObject newEnemy = Instantiate(Enemys[random].gameObject,this.transform.position,this.transform.rotation) as GameObject;
+    void startWave()
+    {
+        Invoke("spawnEnemy", _enemyCoolDown);
+    }
+	void spawnEnemy () 
+    {
+        _currentEnemys++;
+		int random = Random.Range (0, _enemys.Length);
+		GameObject newEnemy = Instantiate(_enemys[random].gameObject,this.transform.position,this.transform.rotation) as GameObject;
 		newEnemy.transform.parent = GameObject.FindGameObjectWithTag ("Enemys").transform;
-		Invoke ("spawnEnemy", coolDown);
+        if(_currentEnemys == _maxEnemys)
+        {
+            _currentEnemys = 0;
+            _wave++;
+            _maxEnemys += _enemyMultiplayer * _wave;
+            
+            Invoke("startWave", _waveCoolDown);
+        }
+        else
+        {
+            Invoke("spawnEnemy", _enemyCoolDown);
+        }
+        
 	}
 }

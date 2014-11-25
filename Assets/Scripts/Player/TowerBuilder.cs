ddff6161beb9f8a7bac109fdd1745b27cd7c40c2
Beau Arthurs <BeauArthurs@gmail.com>
2014-11-25 08:47:59 +0100

    Beau012.2

--- Assets/Scripts/Player/TowerBuilder.cs
+++ Assets/Scripts/Player/TowerBuilder.cs
@@ -95,10 +95,10 @@ public class TowerBuilder : MonoBehaviour {
 	}
     void OnGUI()
     {
-        GUI.DrawTexture(new Rect(20, 20, UiBack.width / 2, UiBack.height / 2), UiBack);
+        GUI.DrawTexture(new Rect(Screen.width /2 - 150, Screen.height - UiBack.height /2, UiBack.width / 2, UiBack.height / 2), UiBack);
         if (_towerToBuild >= 0)
         {
-            GUI.DrawTexture(new Rect(36 + (45 * _towerToBuild), 34, 40, 30), UIButtons[_towerToBuild]);
+            GUI.DrawTexture(new Rect(Screen.width / 2 - 150 + 17 + (45 * _towerToBuild), Screen.height - UiBack.height / 2 + 13, 40, 30), UIButtons[_towerToBuild]);
         }
     }
 }

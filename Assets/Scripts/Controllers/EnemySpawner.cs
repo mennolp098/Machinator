ef844e7bcc22c3455c4a28badbc1d2221990a555
Beau Arthurs <BeauArthurs@gmail.com>
2014-11-26 12:45:35 +0100

    Beau018

--- Assets/Scripts/Controllers/EnemySpawner.cs
+++ Assets/Scripts/Controllers/EnemySpawner.cs
@@ -33,32 +33,21 @@ public class EnemySpawner : MonoBehaviour {
 		int random = Random.Range (0, _enemys.Length);
 		GameObject newEnemy = Instantiate(_enemys[random].gameObject,this.transform.position,this.transform.rotation) as GameObject;
 		newEnemy.transform.parent = GameObject.FindGameObjectWithTag ("Enemys").transform;
-        if(random == 0)
-        {
-<<<<<<< HEAD
-            newEnemy.GetComponent<NormalEnemy>().health += 2 * _wave;
-        }
-        if (random == 1)
-        {
-            newEnemy.GetComponent<FastEnemy>().health += 2 * _wave;
-        }
-        if (random == 2)
+        if (random == 0)
         {
             newEnemy.GetComponent<StrongEnemy>().health += 5 *_wave;
-=======
 			newHealth = 3 * _wave + 7 - (_wave/4);
 			newEnemy.GetComponent<EnemyBehavior>().SetHealth(newHealth);
         }
         if (random == 1)
         {
-			newHealth = 2.5* _wave + 5 - (_wave/4);
+			newHealth = 2.5f* _wave + 5 - (_wave/4);
 			newEnemy.GetComponent<EnemyBehavior>().SetHealth(newHealth);
         }
         if (random == 2)
         {
 			newHealth = 5 *_wave + 10 - (_wave/4);
 			newEnemy.GetComponent<EnemyBehavior>().SetHealth(newHealth);
->>>>>>> origin/master
         }
         if(_currentEnemys == _maxEnemys)
         {
